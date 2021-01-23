0be68c9b3464b79d10c5b25fe05e0d4b58a5d2e6
code-abil <49409749+code-abil@users.noreply.github.com>
2021-01-23 13:23:12 +0530

    Create pairs.cpp

---
+++ STL_1/code/pairs.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pii pair<int,int>
+using namespace std;
+// struct st{
+// 	int first;
+// 	int second;
+// };
+int main(){
+	long long a=1000000000000;
+	ll c=1e10;
+	ll d=1e18;
+	double small=1e-9;
+	ll b=10000000000000;
+	pii newpair=pii(10,20);
+	// pair<int,int>p;
+	// p=make_pair(10,20);
+	// pair<int,char>p;
+	// p=make_pair(10,'a');
+	// cout<<p.first<<" "<<p.second<<"\n";
+	// vector<pair<int,int>>v;
+	// v.push_back(make_pair(10,20));
+	// v.push_back(make_pair(20,30));
+	// //for(int i=0;i<v.size();i++)v[i]
+	// v={1,2,3,4}
+	// //for(auto value:v)value=1,value=2,value=3,value=4
+	// for(auto p:v){
+	// 	cout<<p.first<<" "<<p.second<<"\n";
+	// }
+	pair<int,int>p1=pair<int,int>(10,20);
+	pair<int,int>p2=pair<int,int>(10,20);
+	if(p1<p2)cout<<
+	else cout<<
+}
