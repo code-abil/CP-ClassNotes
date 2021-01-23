4c0611c32c206d913a972ab99c934421ad198326
code-abil <49409749+code-abil@users.noreply.github.com>
2021-01-23 13:21:19 +0530

    Create vectors.cpp

---
+++ STL_1/code/vectors.cpp
@@ -0,0 +1,51 @@
+//#include<iostream>
+//#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+void printelements(vector<int>v){
+	cout<<"Elements:\n";
+	for(int i=0;i<v.size();i++)cout<<v[i]<<" ";cout<<"\n";
+}
+int main(){
+	// vector<int>v;
+	// v.push_back(10);
+	// v.push_back(2);
+	// v.push_back(12);
+	// int n=v.size();
+	// cout<<"Size: "<<n<<"\n";
+	// cout<<"Elements: "<<"\n";
+	// for(int i=0;i<n;i++)cout<<v[i]<<" ";
+	// 	cout<<"\n";
+	// v.pop_back();
+	// cout<<"Size: "<<v.size()<<"\n";
+	// for(int i=0;i<v.size();i++)cout<<v[i]<<" ";
+	// vector<int>v1(10,5);
+	// //cout<<"Size of v1 : "<<v1.size()<<"\n";
+	// printelements(v1);
+	// v=v1;
+	// printelements(v);
+	// int a[10][10];
+	// vector<vector<int> > vectorOfvectors;
+	// vector<vector<int> > 2dvector(10,vector<int>(10,-1));
+	//0 1 2 3 4 5 6. 7 8 9
+	// vector<int>(10) ....
+	//vector<set<int> > vectorOfSets(10,set<int>);
+}
+
+/*
+#include<vector>
+int a[10];
+vector<int>v;
+//vector<data_type> variable_name
+// v={1 2 3 }
+v.push_back(10);
+
+int n=v.size();// 4
+// v={1 2 3 }
+// v={1,2,3,10}
+v.push_back(11);
+// v={1,2,3,10,11};
+
+int n=v.size();// 5
+
+*/
