5dcf1192890a4228ec86f6212211f8b197d54215
code-abil <49409749+code-abil@users.noreply.github.com>
2021-01-23 13:19:10 +0530

    Create queue.cpp

---
+++ STL_1/code/queue.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<queue>
+using namespace std;
+int main(){
+	queue<int>q;
+	q.push(10);
+	cout<<"First Element:";	
+	cout<<q.front()<<"\n";
+	q.pop();
+	cout<<"Size: ";
+	cout<<q.size()<<"\n";
+	//vector<int>v;
+	// v.push_back(10);
+	// v.push_back(11);
+	//v.erase();
+	//cout<<"First Element of vector:";
+	//cout<<v[0]<<"\n";
+}
